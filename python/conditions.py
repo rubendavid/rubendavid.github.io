2cc87b27d14946379ae061d3b45870cb7bd1f84c
Education <education@Rubens-MacBook-Pro.local>
2020-09-07 13:22:28 -0400

    updates

--- python/conditions.py
+++ python/conditions.py
@@ -1,8 +1,8 @@
 n = int(input("Number: "))
 
-if n > 0:
-    print("n is positive")
-elif n < 0:
-    print("n is negative")
+if n > 48:
+    print("Your ass is old")
+elif n < 47:
+    print("You're not that old")
 else:
-    print("n is zero")
+    print("Maybe")
