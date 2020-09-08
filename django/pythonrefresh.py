87646433a2882c5fae3a6986df6c52893909ae2e
rubendavid <ruben@rubendavid.com>
2020-09-08 11:03:27 -0400

    Django lecture 3

--- django/pythonrefresh.py
+++ django/pythonrefresh.py
@@ -96,3 +96,23 @@ print(mydog.age)
 print(Dog.dogInfo)
 print(Dog.dogpond)
 print(Dog.dogdoodoo)
+
+
+def fizzbuzz(range, triggers)
+
+
+range.each do | i|
+result = ''
+triggers.each do | (text, divisor)|
+result << text if i % divisor == 0
+end
+puts result == '' ? i: result
+end
+
+
+end
+
+fizzbuzz(1..100, [
+    ['Fizz', 3],
+    ['Buzz', 5],
+])
