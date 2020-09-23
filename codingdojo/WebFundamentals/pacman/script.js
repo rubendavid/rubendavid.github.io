4e68d01141a03ab31f67d6a0980262c97c3dc273
Education <education@Rubens-MacBook-Pro.local>
2020-09-23 10:08:36 -0400

    updates

--- codingdojo/WebFundamentals/pacman/script.js
+++ codingdojo/WebFundamentals/pacman/script.js
@@ -69,38 +69,32 @@
 
 	document.onkeydown = function (e) {
         if ( e.keyCode == 37 && world[pacman.y][pacman.x - 1] != 2 && pacman.x == 0) {
+          // I found this code on the internet
             document.getElementById("pacman").style.transform = "rotate(-180deg)";
             pacman.x = world[0].length - 1;
-
         } else if (e.keyCode == 37 && world[pacman.y][pacman.x - 1] != 2 && pacman.x > 0) {
+          // I found this code on the internet
             document.getElementById("pacman").style.transform = "rotate(-180deg)";
             pacman.x--;
-
         } else if (e.keyCode == 39 && world[pacman.y][pacman.x + 1] != 2 && pacman.x == world[0].length - 1) {
+          // I found this code on the internet
             document.getElementById("pacman").style.transform = "none";
             pacman.x = 0;
-
         } else if (e.keyCode == 39 && world[pacman.y][pacman.x + 1] != 2) {
+          // I found this code on the internet
             document.getElementById("pacman").style.transform = "none";
             pacman.x++;
-
         } else if (e.keyCode == 38 && world[pacman.y - 1][pacman.x] != 2 && pacman.y > 0) {
+          // I found this code on the internet
             document.getElementById("pacman").style.transform = "rotate(-90deg)";
             pacman.y--;
-
         } else if (e.keyCode == 40 && world[pacman.y + 1][pacman.x] != 2) {
+          // I found this code on the internet
             document.getElementById("pacman").style.transform = "rotate(90deg)";
             pacman.y++;
-
         } if (world[pacman.y][pacman.x] == 1) {
             world[pacman.y][pacman.x] = 0;
             score += 10;
-            displayWorld();
-            displayScore();
-
-        } else if (world[pacman.y][pacman.x] == 3) {
-            world[pacman.y][pacman.x] = 0;
-            score += 50;
             displayWorld();
             displayScore();
         }
